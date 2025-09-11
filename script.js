0877029a4f0238b0dce177c53532cb6a86cc9549
Misfit <78898735+Misfit64@users.noreply.github.com>
2025-09-11 16:14:46 +0530

    Build : playRound function

--- script.js
+++ script.js
@@ -23,8 +23,59 @@ let humanScore = 0, computerScore = 0;
 
 function playRound(humanChoice,computerChoice){
     humanChoice = humanChoice.toLowerCase();
-    if((humanChoice = "rock") && (computerChoice = "paper")){
-        computerScore++;
-        console.log("You lose! Paper beats Rock")
+    if(humanChoice == "rock"){
+        if(computerChoice == "paper"){
+            computerScore++;
+            console.log("You lose! Paper beats Rock");
+        }
+        else if (computerChoice == "scissors"){
+            humanScore++;
+            console.log("You win! Rock beats Scissors");
+        }
+        else if (computerChoice == "rock"){
+            console.log("Tie!");
+        }
+    }
+    else if(humanChoice == "paper"){
+        if(computerChoice == "paper"){
+            console.log("Tie!");
+        }
+        else if (computerChoice == "scissors"){
+            computerScore++;
+            console.log("You lose! Scissors beats Paper");
+        }
+        else if (computerChoice == "rock"){
+            humanScore++;
+            console.log("You win! Paper beats Rock");
+        }
+    }
+    else if(humanChoice == "scissors"){
+        if(computerChoice == "paper"){
+            humanScore++;
+            console.log("You win! Scissors beats Paper");
+        }
+        else if (computerChoice == "scissors"){
+            console.log("Tie!");
+        }
+        else if (computerChoice == "rock"){
+            computerScore++;
+            console.log("You lose! Rock beats Scissors");
+        }
     }
 }
+
+function playGame(){
+    playRound(humanSelection, computerSelection);
+    
+}
+
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(humanSelection);
+console.log(computerSelection);
+
+console.log(`Human Score is ${humanScore}`);
+console.log(`Computer Score is ${computerScore}`);
+playGame();
